b17b55a19d63e1b6ab95740b531ad1cb1af873bc
O^\/^O <abdulaliomarov074@gmail.com>
2026-08-02 22:33:55 +0300

    Added a search by Bodyes and Headings

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
 	"golang.org/x/net/html"
 )
@@ -15,7 +16,11 @@ func downloadHTML(url string) ([]byte, error) {
 		return html, fmt.Errorf("error: URL is empty")
 	}
 
-	resp, err := http.Get(url)
+	client := &http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	resp, err := client.Get(url)
 	if err != nil {
 		return nil, fmt.Errorf("GET %s: %w", url, err)
 	}
@@ -73,9 +78,9 @@ func findTitle(node *html.Node) *html.Node {
 
 	for child := node.FirstChild; child != nil; child = child.NextSibling {
 
-		val := findTitle(child)
-		if val != nil {
-			return val
+		find := findTitle(child)
+		if find != nil {
+			return find
 		}
 
 	}
@@ -83,3 +88,64 @@ func findTitle(node *html.Node) *html.Node {
 	return nil
 }
 
+func extractBody(htmlData []byte) (*html.Node, error) {
+	if len(htmlData) == 0 {
+		return nil, fmt.Errorf("HTML is empty")
+	}
+
+	reader := bytes.NewReader(htmlData)
+
+	root, err := html.Parse(reader)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse HTML: %w", err)
+	}
+
+	body := findBody(root)
+	if body == nil {
+		return nil, fmt.Errorf("Body not found")
+	}
+
+	return body, nil
+
+}
+
+func findBody(node *html.Node) *html.Node {
+	if node == nil {
+		return node
+	}
+	if node.Type == html.ElementNode {
+		if node.Data == "body" {
+			return node
+		}
+	}
+
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		find := findBody(child)
+		if find != nil {
+			return find
+		}
+	}
+
+	return nil
+}
+
+func extractHeadings(body *html.Node) []*html.Node {
+	headings := make([]*html.Node, 0)
+	if body == nil {
+		return headings
+	}
+
+	if body.Type == html.ElementNode {
+		if body.Data == "h1" || body.Data == "h2" || body.Data == "h3" || body.Data == "h4" || body.Data == "h5" || body.Data == "h6" {
+			headings = append(headings, body)
+		}
+	}
+
+	for child := body.FirstChild; child != nil; child = child.NextSibling {
+		childHeadings := extractHeadings(child)
+		headings = append(headings, childHeadings...)
+
+	}
+
+	return headings
+}
